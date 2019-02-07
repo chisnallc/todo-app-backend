32572b1285882454a22e185e5b188558c50cf9e3
chisnallc <chisnallc@hotmail.com>
2019-02-07 16:04:54 +0000

    update to array

--- tasks.js
+++ tasks.js
@@ -4,32 +4,29 @@ const app = express();
 
 app.get('/tasks', function (request, response) {
 
-  const username = request.query.username;
-  const dinner = request.query.tasks;
 
-  const someJson = {
-    message: "Hello" + username + dinner,
-    dinnerList: [{
+
+  const someTasks = [
+    {
       id: 1,
       description: 'Go Shopping',
-      completed: true
+      completed: false
     },
     {
       id: 2,
       description: 'Cook the Food',
-      Completed: false,
+      Completed: false
     },
     {
       id: 3,
       description: 'Eat The Dinner',
       completed: false
-    },
-    ]
-  };
+    }
+  ];
 
-  response.json(someJson);
+  response.json(someTasks);
 
-});
+})
 
 module.exports.handler = serverless(app);
 
